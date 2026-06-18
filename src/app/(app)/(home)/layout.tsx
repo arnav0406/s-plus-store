86f0fc8d072ad5d2f85f7e587bf275f237ddcf3f
Arnav Singh Rathore <asrathore0406@gmail.com>
2026-06-18 23:00:22 +0000

    Implement mobile responsive category sidebar and add routing links

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -5,41 +5,43 @@ import { Footer } from "./footer";
 import { Navbar } from "./navbar";
 import { SearchFilters } from './search-filters';
 import { Category } from '@/payload-types';
+import { CustomCategory } from './search-filters/types';
 
 interface Props {
     children: React.ReactNode;
 };
 
-const Layout = async ({children}:Props) => {
+const Layout = async ({ children }: Props) => {
     const payload = await getPayload({
         config: configPromise,
     })
     const data = await payload.find({
         collection: 'categories',
-        depth:1, // Populate subcategories, subcategories.[0] will be a type of "Category"
-        pagination:false,
-        where:{
-            parent:{
-                exists:false,
+        depth: 1, // Populate subcategories, subcategories.[0] will be a type of "Category"
+        pagination: false,
+        where: {
+            parent: {
+                exists: false,
             },
         },
+        sort: "name",
     });
-    const formattedData = data.docs.map((doc) =>({
+    const formattedData: CustomCategory[] = data.docs.map((doc) => ({
         ...doc,
-        subcategories: (doc.subcategories?.docs ?? []).map((doc)=>({
+        subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
             // Because of "depth:1" we are confident "doc" will be a type of "Category"
             ...(doc as Category),
-            subcategories:undefined,
+            subcategories: undefined,
         }))
     }));
-    return(
+    return (
         <div className="flex flex-col min-h-screen">
-            <Navbar/>
-            <SearchFilters data={formattedData}/>
-                <div className="flex-1 bg-[#F4F4F0]">
-                    {children}
-                </div>    
-            <Footer/>
+            <Navbar />
+            <SearchFilters data={formattedData} />
+            <div className="flex-1 bg-[#F4F4F0]">
+                {children}
+            </div>
+            <Footer />
         </div>
     );
 }
--- src/app/(app)/(home)/search-filters/subcategory-menu.tsx
+++ src/app/(app)/(home)/search-filters/subcategory-menu.tsx
@@ -1,17 +1,17 @@
 import { Category } from "@/payload-types";
 import Link from "next/link";
 
-interface Props{  
+interface Props {
     category: Category;
-    isOpen:boolean;
-    position:{top:number; left: number};
+    isOpen: boolean;
+    position: { top: number; left: number };
 }
 export const SubcategoryMenu = ({
     category,
     isOpen,
     position,
-}:Props)=>{
-    if(!isOpen || !category.subcategories || category.subcategories.length === 0){
+}: Props) => {
+    if (!isOpen || !category.subcategories || category.subcategories.length === 0) {
         return null;
     }
 
@@ -19,29 +19,29 @@ export const SubcategoryMenu = ({
 
     return (
         <div
-        className="fixed z-100"
-        style={{
-            top: position.top,
-            left: position.left,
-        }}
+            className="fixed z-100"
+            style={{
+                top: position.top,
+                left: position.left,
+            }}
         >
-            <div className="h-3 w-60"/>
-            <div 
-            style={{backgroundColor}}
-            className="w-60 text-black rounded-md overflow-hidden border shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[2px] -translate-y-[2px]">
+            <div className="h-3 w-60" />
+            <div
+                style={{ backgroundColor }}
+                className="w-60 text-black rounded-md overflow-hidden border shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[2px] -translate-y-[2px]">
                 <div>
-                    {category.subcategories?.map((subcategory: Category) =>(
+                    {category.subcategories?.map((subcategory: Category) => (
                         <Link
                             key={subcategory.slug}
-                            href="/"
+                            href={`/${category.slug}/${subcategory.slug}`}
                             className="w-full text-left p-4 hover:bg-black hover:text-white flex justify-between items-center font-medium"
-                            >
-                                {subcategory.name}
-                            </Link>
+                        >
+                            {subcategory.name}
+                        </Link>
                     ))}
                 </div>
             </div>
         </div>
     )
-    
+
 }
