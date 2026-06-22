7f0fcd1300690ae0a2569e6b7831f31a76005d69
Arnav Singh Rathore <asrathore0406@gmail.com>
2026-06-22 14:26:55 +0000

    trpc integration fixed

--- src/app/(app)/(home)/search-filters/categories-sidebar.tsx
+++ src/app/(app)/(home)/search-filters/categories-sidebar.tsx
@@ -10,7 +10,7 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useTRPC } from "@/trpc/client";
 import { useQuery } from "@tanstack/react-query";
-import { CategoriesGetManyOutput, CategoriesGetManyOutputSingle } from "@/modules/categories/types";
+import { CategoriesGetManyOutput } from "@/modules/categories/types";
 
 interface Props {
     open: boolean;
