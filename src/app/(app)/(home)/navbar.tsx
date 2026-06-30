2a777d18cf65f48cc8875b301c03526248b58e56
Arnav Singh Rathore <asrathore0406@gmail.com>
2026-06-30 06:14:34 +0000

    added authentication

--- src/app/(app)/(home)/navbar.tsx
+++ src/app/(app)/(home)/navbar.tsx
@@ -12,19 +12,19 @@ import { MenuIcon } from "lucide-react";
 
 
 const poppins = Poppins({
-    subsets:["latin"],
-    weight:["700"]
+    subsets: ["latin"],
+    weight: ["700"]
 });
 interface NavbarItemProps {
-    href:string;
+    href: string;
     children: React.ReactNode;
-    isActive?:boolean;
+    isActive?: boolean;
 }
 const NavbarItem = ({
     href,
     children,
     isActive,
-}:NavbarItemProps) =>{
+}: NavbarItemProps) => {
     return (
         <Button
             asChild
@@ -32,79 +32,79 @@ const NavbarItem = ({
             className={cn(
                 "bg-transparent hover:bg-transparent rounded-full hover:border-primary border-transparent px-3.5 text-lg",
                 isActive && "bg-black text-white hover:bg-black hover:text-white",
-    )}
+            )}
         >
             <Link href={href}>
-            {children}
+                {children}
             </Link>
         </Button>
     );
 };
 
 const navbarItems = [
-    {href:"/",children:"Home"},
-    {href:"/about",children:"About"},
-    {href:"/features",children:"Features"},
-    {href:"/pricing",children:"Pricing"},
-    {href:"/contact",children:"Contact"},
+    { href: "/", children: "Home" },
+    { href: "/about", children: "About" },
+    { href: "/features", children: "Features" },
+    { href: "/pricing", children: "Pricing" },
+    { href: "/contact", children: "Contact" },
 ];
 
-export const Navbar = () =>{
+export const Navbar = () => {
     const pathname = usePathname();
-    const [isSidebarOpen,setIsSidebarOpen]=useState(false);
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
     return (
         <nav className="h-20 flex border-b justify-between font medium bg-white">
             <Link href="/" className="pl-6 flex items-center">
-                <span className={cn("text-5xl font-semibold",poppins.className)}>
-                  S+ Store
+                <span className={cn("text-5xl font-semibold", poppins.className)}>
+                    S+ Store
                 </span>
             </Link>
             <NavbarSidebar
-            items={navbarItems}
-            open={isSidebarOpen}
-            onOpenChange={setIsSidebarOpen}
+                items={navbarItems}
+                open={isSidebarOpen}
+                onOpenChange={setIsSidebarOpen}
             />
             <div className="items-center gap-4 hidden lg:flex">
-                {navbarItems.map((item)=>(
-                    <NavbarItem 
-                    key={item.href}
-                    href={item.href}
-                    isActive={pathname===item.href}
+                {navbarItems.map((item) => (
+                    <NavbarItem
+                        key={item.href}
+                        href={item.href}
+                        isActive={pathname === item.href}
                     >
-                    {item.children}
+                        {item.children}
                     </NavbarItem>
                 ))}
 
             </div>
             <div className="hidden lg:flex ">
-                <Button 
-                asChild
-                variant="secondary"
-                className="border-l border-t-0 border-r-0 border-b-0 h-full px-12 rounded-none bg-white hover:bg-pink-400 text-lg transition-colors">
-                    <Link href="/sign-in">
+                <Button
+                    asChild
+                    variant="secondary"
+                    className="border-l border-t-0 border-r-0 border-b-0 h-full px-12 rounded-none bg-white hover:bg-pink-400 text-lg transition-colors">
+                    <Link prefetch href="/sign-in">
                         Log in
                     </Link>
                 </Button>
                 <Button
-                asChild
-                variant="secondary"
-                className="border-l border-t-0 border-r-0 border-b-0 h-full px-12 rounded-none bg-black text-white hover:bg-pink-400 hover:text-black text-lg transition-colors"
+                    asChild
+                    variant="secondary"
+                    className="border-l border-t-0 border-r-0 border-b-0 h-full px-12 rounded-none bg-black text-white hover:bg-pink-400 hover:text-black text-lg transition-colors"
                 >
-                    <Link href="/sign-up">
+                    <Link prefetch href="/sign-up">
                         Start Selling
-                    </Link>                    
+                    </Link>
                 </Button>
             </div>
             <div
-            className="flex lg:hidden items-center justify-center"
+                className="flex lg:hidden items-center justify-center"
             >
                 <Button
-                variant="ghost"
-                className="size-12 border-transparent bg-white"
-                onClick={()=>setIsSidebarOpen(true)}
+                    variant="ghost"
+                    className="size-12 border-transparent bg-white"
+                    onClick={() => setIsSidebarOpen(true)}
                 >
-                <MenuIcon/>
+                    <MenuIcon />
                 </Button>
             </div>
         </nav>
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,9 @@
 import { createTRPCRouter } from '../init';
 import { categoryRouter } from '@/modules/categories/server/procedure';
+import { authRouter } from '@/modules/auth/server/procedure';
 
 export const appRouter = createTRPCRouter({
+    auth: authRouter,
     categories: categoryRouter
 });
 
