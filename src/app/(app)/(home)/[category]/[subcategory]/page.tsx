e517c852b7cd5f7dfc3f360fba857f83bb9a88ef
Arnav Singh Rathore <asrathore0406@gmail.com>
2026-07-06 05:48:10 +0000

    added products logic

--- src/app/(app)/(home)/[category]/[subcategory]/page.tsx
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -1,7 +1,34 @@
+import { ProductList, ProductListSkeletton } from "@/modules/products/ui/components/product-list";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+
+interface Props {
+    params: Promise<{
+        subcategory: string;
+    }>
+};
 
 
-const Page = () => {
-    return ("Subcategory");
-}
+const Page = async ({ params }: Props) => {
+
+    const { subcategory } = await params;
+
+    const queryClient = getQueryClient();
+
+    void queryClient.prefetchQuery(
+        trpc.products.getMany.queryOptions({
+            category: subcategory,
+        })
+    );
+
+    return (
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Suspense fallback={<ProductListSkeletton />}>
+                <ProductList category={subcategory} />
+            </Suspense>
+        </HydrationBoundary>
+    );
+};
 
 export default Page;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,11 @@
 import { createTRPCRouter } from '../init';
 import { categoryRouter } from '@/modules/categories/server/procedure';
 import { authRouter } from '@/modules/auth/server/procedure';
+import { productsRouter } from '@/modules/products/server/procedure';
 
 export const appRouter = createTRPCRouter({
     auth: authRouter,
+    products: productsRouter,
     categories: categoryRouter
 });
 
