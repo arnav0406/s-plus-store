ae80ca3b9ba830ad44a4c1b1e5965c741c4f0769
Arnav Singh Rathore <asrathore0406@gmail.com>
2026-07-13 03:46:27 +0000

    added checkout page and its logic

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -4,13 +4,15 @@ import { authRouter } from '@/modules/auth/server/procedure';
 import { productsRouter } from '@/modules/products/server/procedure';
 import { tagsRouter } from '@/modules/tags/server/procedure';
 import { tenantsRouter } from '@/modules/tenants/server/procedure';
+import { checkoutRouter } from '@/modules/checkout/server/procedure';
 
 export const appRouter = createTRPCRouter({
     auth: authRouter,
     tags: tagsRouter,
     tenants: tenantsRouter,
     products: productsRouter,
-    categories: categoryRouter
+    categories: categoryRouter,
+    checkout: checkoutRouter
 });
 
 // export type definition of API
